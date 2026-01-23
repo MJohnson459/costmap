59a6099db53e20c1613ec4a7c00c52e355f4c0b9
Michael Johnson <mjohnson459@gmail.com>
2026-01-23 16:39:18 +0000

    Cleanup unused code

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,6 @@ pub mod grid;
 pub mod loaders;
 pub mod raycast;
 pub mod types;
-pub mod visualization;
 
 pub use grid::{OccupancyGrid, VoxelGrid};
 pub use loaders::ros2::{RosMapLoader, RosMapMetadata};
